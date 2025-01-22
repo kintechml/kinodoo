6977355e09fe55594328ef0d336c75c26acc0eac
warsic <warsicdia@gmail.com>
2025-01-22 19:25:50 +0000

    [17.0] FIX: filter product by part number

--- eak_stock/models/product_template.py
+++ eak_stock/models/product_template.py
@@ -16,3 +16,11 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
     x_sap_code = fields.Char(related='product_tmpl_id.x_sap_code', string='SAP Code', readonly=False)
     x_part_number = fields.Char(related='product_tmpl_id.x_part_number', string='Part Number', readonly=False)
+
+    @api.model
+    def _name_search(self, name, domain=None, operator='ilike', limit=None, order=None):
+        domain = domain or []
+        if name:
+            domain += ['|', ('name', operator, name),
+                     ('x_part_number', operator, name)]
+        return self._search(domain, limit=limit, order=order)
